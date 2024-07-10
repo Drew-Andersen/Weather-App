aea0db8b7a8a963fdda90ca7bbf362f649b9e255
Drew-Andersen <andrewtandersen@gmail.com>
2024-07-10 11:12:36 -0400

    Added the nexessary folders and files

---
+++ assets/js/script.js
@@ -0,0 +1,7 @@
+// Added the inputs for city form, histroy, current weather, and the 5-day forcast
+const inputCity = $('#city-input');
+const formCity = $('#city-form');
+const history = $('#city-history');
+const currentWeather = $('#current-weather');
+const weatherForcast = $('#weather-forcast');
+
