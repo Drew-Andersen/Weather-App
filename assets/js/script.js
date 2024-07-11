b338e0f353f706dd2154e48d0a9d91a98ba51c5b
Drew-Andersen <andrewtandersen@gmail.com>
2024-07-11 16:04:59 -0400

    5 Day forcast now works

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,9 +6,9 @@ const forcastWeatherDiv = $('#weather-forcast');
 const historyDiv = $('#city-history');
 
 
-function getCities () {
-    const storedCities = JSON.parse(localStorage.getItem('cities')); 
-    if(storedCities !== null){
+function getCities() {
+    const storedCities = JSON.parse(localStorage.getItem('cities'));
+    if (storedCities !== null) {
         return storedCities;
     } else {
         const emptyCity = [];
@@ -16,16 +16,16 @@ function getCities () {
     }
 }
 
-function saveCities (cities) {
+function saveCities(cities) {
     const tempCities = getCities();
     tempCities.push(cities);
     localStorage.setItem('cities', JSON.stringify(tempCities));
 }
 
-function displayCities () {
+function displayCities() {
     historyDiv.empty();
     const tempCities = getCities();
-    tempCities.forEach((city,i) => {
+    tempCities.forEach((city, i) => {
         const cityCard = $('<div>');
         cityCard.addClass('city-card bg-secondary text-white text-center border border-light rounded');
         cityCard.attr('data-id', i);
@@ -37,26 +37,27 @@ function displayCities () {
 function getData() {
     let city = inputCity.val().trim();
     const apiURL = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}&units=imperial`;
+    https://api.openweathermap.org/data/2.5/weather?q={city}&appid={apiKey}&
 
     fetch(apiURL)
-    .then(function (response) {
-      return response.json();
-    })
-    .then(function (data) {
-      console.log(data);
-      currentWeatherDiv.empty();
-      renderCurrentWeather(city,data);
-      saveCities(city);
-      displayCities();
-    //   renderForcastWeather(city,data);
-    })
-    .catch(function (error) {
-      console.log(error);
-    });
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            console.log(data);
+            currentWeatherDiv.empty();
+            renderCurrentWeather(city, data);
+            saveCities(city);
+            displayCities();
+            renderForcastWeather(city,data);
+        })
+        .catch(function (error) {
+            console.log(error);
+        });
 }
 
-function renderCurrentWeather (city,weather) {
-    const cityName = city.name;
+function renderCurrentWeather(city, weather) {
+    const cityName = weather.city.name;
     const curDate = dayjs().format('M/D/YYYY');
     const temp = weather.list[0].main.temp;
     const wind = weather.list[0].wind.speed;
@@ -64,6 +65,7 @@ function renderCurrentWeather (city,weather) {
     const icon = weather.list[0].weather[0].icon;
     const iconURL = `https://openweathermap.org/img/wn/${icon}.png`;
 
+    const weatherDiv = $('<div>').addClass('card col-10 m-auto');
     const cityNameh4 = $('<h4>');
     const temph6 = $("<h6>");
     const windH6 = $('<h6>');
@@ -77,16 +79,43 @@ function renderCurrentWeather (city,weather) {
     iconImg.attr('src', iconURL);
 
     cityNameh4.append(iconImg)
-    currentWeatherDiv.append(cityNameh4, temph6, windH6, humidityH6);
+    weatherDiv.append(cityNameh4, temph6, windH6, humidityH6);
+    currentWeatherDiv.append(weatherDiv);
 }
 
 //ToDo 
-// function renderForcastWeather (city,weather) {
-//     // similar to the renderCurrentWeather
-//     console.log('forcast funciton'); // Is working
-// }
-    
-function handleSearchHistory () {
+function renderForcastWeather (city,weather) {
+    for (let i = 0; i <5; i++) {
+        // Add a div to hold the card elements
+        const forcastCard = $('<div>');
+        forcastCard.addClass('card col-2');
+        // Add a date for the card
+        const forcastDate = $('<p>');
+        forcastDate.text(dayjs().add(i + 1, 'day').format('M/D/YYYY'));
+        // Create weather icon
+        const weatherIcon = $('<img>');
+        const icon = weather.list[i].weather[0].icon;
+        weatherIcon.attr('src', `https://openweathermap.org/img/wn/${icon}@2x.png`);
+        // Creat temp component
+        const temp = weather.list[i].main.temp;
+        const tempDiv = $('<p>');
+        tempDiv.text('Temp: ' + temp + '°F');
+        // Create wind component
+        const wind = weather.list[i].wind.speed;
+        const windDiv = $('<p>');
+        windDiv.text('Wind: ' + wind + 'mph');
+        // Add humidity component
+        const humid = weather.list[i].main.humidity;
+        const humidDiv = $('<p>');
+        humidDiv.text('Humidity: ' + humid + '%');
+
+        // Append them to the page
+        forcastCard.append(forcastDate, weatherIcon, tempDiv, windDiv, humidDiv);
+        forcastWeatherDiv.append(forcastCard);
+    }
+}
+
+function handleSearchHistory() {
     cityID = $(this).attr('data-id');
     let savedCities = getCities();
     currentWeatherDiv.empty();
